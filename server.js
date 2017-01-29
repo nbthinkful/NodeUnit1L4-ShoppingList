4e42035b188674a8de34bd56f7c713f0671b6728
nbthinkful <nbonavia@thinkful.com>
2017-01-29 15:55:58 +0100

    Finished Lesson 5

--- server.js
+++ server.js
@@ -43,6 +43,37 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/shopping-list/:id', jsonParser, (req, res) => {
+  validation = validator.validate(req, 'name','budget','id');
+
+  if (!validation.isValid) {
+    console.error(validation.message);
+    return res.status(400).send(validation.message);
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  const updatedItem = ShoppingList.update({
+    id: req.params.id,
+    name: req.body.name,
+    budget: req.body.budget
+  });
+  res.status(204).json(updatedItem);
+});
+
 app.delete('/shopping-list/:id', (req, res) => {
   ShoppingList.delete(req.params.id);
   console.log(`Deleted shopping list item \`${req.params.id}\``);
@@ -63,6 +94,32 @@ app.post('/recipes', jsonParser, (req, res) => {
 
   res.json(Recipes.create(req.body.name,req.body.ingredients));
 });
+
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  validation = validator.validate(req, 'name','ingredients','id');
+
+  if (!validation.isValid) {
+    console.error(validation.message);
+    return res.status(400).send(validation.message);
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  console.log(`Updating Recipe item [${req.params.id}]`);
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  
+  res.status(204).json(updatedItem);
+})
 
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
